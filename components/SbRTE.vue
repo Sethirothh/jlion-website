79308d5369c5b822b8bc71544c12d24316520626
Jesper <jespergyldenlve@gmail.com>
2022-10-18 20:33:58 +0200

    JLION-3: RTE styling + Footer

--- components/SbRTE.vue
+++ components/SbRTE.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="rte">
     <rich-text-renderer :document="richtext" />
   </div>
 </template>
@@ -9,3 +9,96 @@ export default {
   props: ["richtext"],
 };
 </script>
+<style lang="scss" scoped>
+ul,
+ol {
+  margin: 14px 16px;
+  ul {
+    margin: 8px 32px;
+  }
+}
+blockquote {
+  font-size: $font-size-quote;
+  padding: 14px 22px;
+  width: 100%;
+  background-color: rgba($color-gray, 0.4);
+  margin: 14px 0;
+  border-radius: 4px;
+
+  &::before {
+    content: "|";
+    display: inline;
+    color: $color-gray;
+  }
+  p {
+    display: inline;
+    margin-left: 4px;
+  }
+}
+hr {
+  margin: 48px 12px;
+}
+h1 {
+  font-size: $font-size-heading;
+  line-height: 1.6em;
+}
+h2 {
+  font-size: $font-size-heading-2;
+  line-height: 1.4em;
+}
+h3 {
+  font-size: $font-size-heading-3;
+  line-height: 1.3em;
+}
+h4 {
+  font-size: $font-size-subheading;
+  line-height: 1.2em;
+}
+h5 {
+  font-size: $font-size-subheading-2;
+  line-height: 1.2em;
+}
+h6 {
+  font-size: $font-size-subheading-3;
+  line-height: 1.2em;
+}
+p {
+  margin: 8px 0;
+  font-size: $font-size-paragraph;
+}
+i {
+  font-weight: 100;
+}
+b {
+  font-weight: 900;
+}
+a {
+  text-decoration: none;
+  overflow: hidden;
+  display: inline-block;
+  position: relative;
+  color: $color-primary;
+  font-weight: 600;
+  margin-bottom: -5px;
+
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+    bottom: 1px;
+    left: 0;
+    width: 100%;
+    overflow-wrap: break-word;
+    height: 1px;
+    background-color: $color-primary;
+    transition: 0.2s ease;
+    transform: translateX(-100%);
+  }
+
+  &:hover {
+    &::after {
+      transform: translateX(0);
+    }
+  }
+}
+</style>
--- components/bloks/TextBlock.vue
+++ components/bloks/TextBlock.vue
@@ -1,7 +1,14 @@
 <template>
-  <div v-editable="blok">
+  <div v-editable="blok" class="text-block">
     <h1>{{ blok.heading }}</h1>
     <SbRTE :richtext="blok.paragraph"></SbRTE>
+    <div class="text-block__img-wrapper">
+      <img
+        :src="blok.image.filename"
+        :alt="blok.image.alt"
+        class="text-block__img"
+      />
+    </div>
   </div>
 </template>
 
@@ -15,3 +22,24 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.text-block {
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 60px 14px;
+  width: 100%;
+  h1 {
+    font-size: $font-size-heading;
+    border-bottom: 1px solid $color-gray;
+    margin-bottom: 24px;
+  }
+  &__img-wrapper {
+    padding-top: 24px;
+    margin-top: 24px;
+    border-top: 1px solid $color-gray;
+  }
+  &__img {
+    width: 100%;
+  }
+}
+</style>
